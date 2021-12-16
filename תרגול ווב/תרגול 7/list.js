ee39b7bda26339fd77deb4ba7497c766cfe03ef2
hajajmaor <hajajmaor@gmail.com>
2021-12-16 19:05:27 +0200

    תרגול 8

--- תרגול ווב/תרגול 7/list.js
+++ תרגול ווב/תרגול 7/list.js
@@ -1,7 +1,18 @@
 let ul = document.getElementById("list")
-
+// alert("Hello 16/12/2021");
+document.write(10 * 0.3);
 function nameOfFunction() {
     let itemText = document.getElementById("item")
     ul.innerHTML += "<li>" + itemText.value + "</li>"
     itemText.value = ""
+}
+function calculateString() {
+    let stringOfCalculations = document.getElementById("inputColshuw");
+    try {
+        // eval(stringOfCalculations.value);
+        let result = eval("5*6*2/");
+        document.write(result);
+    } catch (error) {
+        document.write("Error in eval function")
+    }
 }
